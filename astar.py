4ecea44ce1803e8f2f7089e9e00243ed31e3a7d9
Phat <minhphatnguyen6@gmail.com>
2022-10-14 22:22:51 +0700

    astar checked

--- astar.py
+++ astar.py
@@ -15,7 +15,7 @@ red = Red()
 def heuristic(a, b):
     (x1, y1) = a
     (x2, y2) = b
-    return math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+    return math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))/24
 
 def astar(x,y,end_x,end_y, neighbor, cost):
     visited = []
