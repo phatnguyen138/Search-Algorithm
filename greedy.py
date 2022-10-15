6c63ca4770056fcee67a70a7ac7d5d6e7f1b9159
Quanhcmus <daicalun61@gmail.com>
2022-10-16 01:09:39 +0700

    fix greedy.py and add map2.txt

--- greedy.py
+++ greedy.py
@@ -7,26 +7,25 @@ from heuristic import *
 def greedy_bfs(x,y,x_end,y_end,neighbor,cost=0):
     green = Green()
     blue = Blue()
+    red=Red()
     visited=[]
     slution={}
     frontier=PriorityQueue()
     frontier.put((x,y),0)
-    # came_from={}
-    # came_from[(x,y)]=None
     while not frontier.empty():
         current=frontier.get()
         if current==(x_end,y_end):
             break
-        priority_list=[]
+        red.goto(current)
         for next in neighbor[current]:
             if next not in visited:
-                priority_list.append(heuristic(next,(x_end,y_end)))
-                frontier.put(next,heuristic(next,(x_end,y_end)))
+                priority = heuristic(next,(x_end,y_end))
+                frontier.put(next,priority)
+                visited.append(next)
                 slution[next]=current
                 blue.goto(next)
                 blue.stamp()
         green.goto(current)
         green.stamp()
-        visited.append(current)
     return slution
 
